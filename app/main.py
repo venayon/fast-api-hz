1e625f540ca55639165249815795a58287018479
rana <rana@dhanya.com>
2023-10-02 00:01:36 +0100

    Initial changes

--- app/main.py
+++ app/main.py
@@ -10,15 +10,14 @@ from typing import Union
 import logging
 import hazelcast
 
-
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI(title="Items API", openapi_url="/openapi.json")
 
-
 logging.basicConfig(level=logging.INFO)
 
 hazelcast_address = os.environ.get('hazelcast_address', 'localhost:5701')
+
 
 class Item(BaseModel):
     name: str
@@ -28,14 +27,16 @@ class Item(BaseModel):
 
     def id(self):
         return self.name
+
+
 client = hazelcast.HazelcastClient(cluster_members=[hazelcast_address])
 
 # Initialize the client configuration
-#config = hazelcast.ClientConfig()
+# config = hazelcast.ClientConfig()
 # Use the hazelcast_address for the network configuration
-#config.network_config.addresses.append(hazelcast_address)
+# config.network_config.addresses.append(hazelcast_address)
 # Start the Hazelcast client with the provided configuration
-#client = hazelcast.HazelcastClient(config)
+# client = hazelcast.HazelcastClient(config)
 items_map = client.get_map("items").blocking()
 
 
@@ -52,17 +53,10 @@ async def create_item(item: Item):
     items_map.put(id, HazelcastJsonValue(item.model_dump()))
     return id
 
-
 @app.get("/items/")
 def read_all_item():
     return items_map.key_set()
 
 
-
-
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info")
-    # uvicorn app.main:app --reload --host 0.0.0.0 --port 8001 --log-level info
-    print("I am here.")
